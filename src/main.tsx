4550ded5f4405e152414d9b4d1bd941cbfb2c851
dtfgenetics <green801028@gmail.com>
2026-09-03 04:59:57 -0500

    Visual polish: Grow Doc dark botanical scientific UI
    * style: add Grow Doc dark botanical visual system
    
    * style: load Grow Doc visual polish layer
    
    * style: finish Grow Doc secondary view polish
    
    * style: load complete Grow Doc view polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './visual-observations.css'
+import './growdoc-visual-polish.css'
+import './growdoc-visual-polish-views.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
