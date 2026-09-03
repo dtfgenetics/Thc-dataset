ab710be475430916be6ff93fbd02f5edf82ee7b0
dtfgenetics <green801028@gmail.com>
2026-09-03 04:39:54 -0500

    style: load Grow Doc visual polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './visual-observations.css'
+import './growdoc-visual-polish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
