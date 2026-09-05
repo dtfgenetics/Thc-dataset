26e2734acd15beebdbfcb7def26235cd67ac0894
dtfgenetics <green801028@gmail.com>
2026-09-05 15:10:13 -0500

    Grow Doc: load secondary view v3 overrides

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './growdoc-visual-polish.css'
 import './growdoc-visual-polish-views.css'
 import './growdoc-mobile-containment.css'
 import './growdoc-visual-system-v3.css'
+import './growdoc-secondary-views-v3.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
