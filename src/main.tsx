e8cf19548210b16bc48737aa203e5ee697989d94
dtfgenetics <green801028@gmail.com>
2026-09-15 17:30:44 -0500

    Use consolidated Grow Doc production stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './visual-observations.css'
-import './growdoc-visual-polish.css'
-import './growdoc-visual-polish-views.css'
-import './growdoc-mobile-containment.css'
-import './growdoc-visual-system-v3.css'
-import './growdoc-secondary-views-v3.css'
-import './growdoc-shell-v3.css'
-import './growdoc-mobile-render-refinement-v1.css'
+import './growdoc-production.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
